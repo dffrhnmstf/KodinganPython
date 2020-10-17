6588aab3495e35b10be343317e5e2859b085bb85
Daffa Raihan Mustofa <62093651+dffrhnmstf@users.noreply.github.com>
2020-10-17 10:44:35 +0700

    Add files via upload

---
+++ repl1.py
@@ -0,0 +1,11 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+n1 = int(input("masukkan data 1 :"))
+n2 = int(input("masukkan data 2 :"))
+n3 = int(input("masukkan data 3 :"))
+rata_rata = n1 + n2 + n3 / 3
+print(rata_rata)
+
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+print(5, 10, 15, 20, 25, sep="---")
